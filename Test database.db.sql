90037e636d12a9913eea75dab129d83932d0ca86
rcruanes <148401359+rcruanes@users.noreply.github.com>
2024-02-20 14:11:20 -0800

    Update Test database.db.sql
    First table updated

--- Test database.db.sql
+++ Test database.db.sql
@@ -2,10 +2,29 @@ BEGIN TRANSACTION;
 CREATE TABLE IF NOT EXISTS "Recommendations" (
 	"Recommendation"	VARCHAR(255) NOT NULL
 );
+CREATE TABLE IF NOT EXISTS "Personal" (
+	"p_name"	VARCHAR(255),
+	"p_education"	VARCHAR(255),
+	"p_type"	VARCHAR(255),
+);
+CREATE TABLE IF NOT EXISTS "Education" (
+	"First Name"	VARCHAR(255),
+	"Last Name"	VARCHAR(255),
+	"School"	VARCHAR(255),
+	"Degree"	VARCHAR(255)
+);
+CREATE TABLE IF NOT EXISTS "Prof" (
+	"First Name"	VARCHAR(255),
+	"Last Name"	VARCHAR(255),
+	"School"	VARCHAR(255),
+	"Degree"	VARCHAR(255)
+);
 CREATE TABLE IF NOT EXISTS "Nominee Information" (
 	"First Name"	VARCHAR(255),
 	"Last Name"	VARCHAR(255),
 	"School"	VARCHAR(255),
 	"Degree"	VARCHAR(255)
 );
+
+
 COMMIT;
