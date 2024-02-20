66f259b5de387c5c7d1deec7ce697358422b61ab
LaserCrafter <elvisbui36@gmail.com>
2024-02-20 14:21:31 -0800

    Updated Tables with entities for each individual question

--- Test database.db.sql
+++ Test database.db.sql
@@ -1,11 +1,19 @@
 BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS "Recommendations" (
-	"Recommendation"	VARCHAR(255) NOT NULL
+CREATE TABLE IF NOT EXISTS "Employee" (
+	"e_name"	VARCHAR(255) NOT NULL, 
+	"e_school"	VARCHAR(255) NOT NULL,
+	"e_type"	VARCHAR(255) NOT NULL
 );
-CREATE TABLE IF NOT EXISTS "Nominee Information" (
-	"First Name"	VARCHAR(255),
-	"Last Name"	VARCHAR(255),
-	"School"	VARCHAR(255),
-	"Degree"	VARCHAR(255)
+CREATE TABLE IF NOT EXISTS "Individual" (
+	"i_performance"	VARCHAR(255) NOT NULL,
+	"i_contribution"	VARCHAR(255) NOT NULL,
+);
+CREATE TABLE IF NOT EXISTS "Project" (
+	"p_performance"	VARCHAR(255) NOT NULL,
+	"p_development"	VARCHAR(255) NOT NULL,
+);
+CREATE TABLE IF NOT EXISTS "People" (
+	"pe_performance"	VARCHAR(255) NOT NULL,
+	"pe_development"	VARCHAR(255) NOT NULL,
 );
 COMMIT;
