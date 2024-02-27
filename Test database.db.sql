610fc810ce397c2075ec565c5d4df6e916d1bead
rcruanes <148401359+rcruanes@users.noreply.github.com>
2024-02-27 14:17:12 -0800

    Update Test database.db.sql

--- Test database.db.sql
+++ Test database.db.sql
@@ -1,18 +1,18 @@
 BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS "Employee" (
+CREATE TABLE IF NOT EXISTS "employee" (
 	"e_name"	VARCHAR(255) NOT NULL, 
 	"e_school"	VARCHAR(255) NOT NULL,
 	"e_type"	VARCHAR(255) NOT NULL
 );
-CREATE TABLE IF NOT EXISTS "Individual" (
+CREATE TABLE IF NOT EXISTS "individual" (
 	"i_performance"	VARCHAR(255) NOT NULL,
 	"i_contribution"	VARCHAR(255) NOT NULL,
 );
-CREATE TABLE IF NOT EXISTS "Project" (
+CREATE TABLE IF NOT EXISTS "project" (
 	"p_performance"	VARCHAR(255) NOT NULL,
 	"p_development"	VARCHAR(255) NOT NULL,
 );
-CREATE TABLE IF NOT EXISTS "People" (
+CREATE TABLE IF NOT EXISTS "people" (
 	"pe_performance"	VARCHAR(255) NOT NULL,
 	"pe_development"	VARCHAR(255) NOT NULL,
 );
