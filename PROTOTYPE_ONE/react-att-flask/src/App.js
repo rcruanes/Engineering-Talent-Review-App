d37a64de4eb4dec8fc2c5570b53994366336c22f
Eric Casillas <158228182+hero346@users.noreply.github.com>
2024-05-03 15:22:29 -0700

    Add files via upload

--- PROTOTYPE_ONE/react-att-flask/src/App.js
+++ PROTOTYPE_ONE/react-att-flask/src/App.js
@@ -1,54 +1,49 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Register from './components/Register';
+import Dashboard from './components/Dashboard';
+import NominatorDashboard from './components/NominatorDashboard';
+import RecommenderDashboard from './components/RecommenderDashboard';
+import ReviewCommitteeDashboard from './components/ReviewCommitteeDashboard';
+import NewNomineeForm from './components/NewNomineeForm';
+import ViewNominee from './components/ViewNominee';
+import EditNominee from './components/EditNominee';
+import RecommendNominee from './components/RecommendNominee';
+import NomineeDetails from './components/NomineeDetails';
+import NomineeEducationForm from './components/NomineeEducationForm';
+import EditEducationForm from './components/EditEducationForm'; 
+import ProtectedRoute from './components/ProtectedRoute';  
 
 function App() {
-  const [formData, setFormData] = useState({
-    first_name: '',
-    last_name: '',
-    email: '',
-    pw: '',
-    confirm_pw: '',
-    role: ''
-  });
-
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value
-    });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      console.log('Form Data:', formData); // Log the form data before sending the request
-      const response = await axios.post('http://127.0.0.1:5000/user/create', formData);
-      console.log('User created:', response.data);
-      // Optionally, handle success response (e.g., redirect to dashboard)
-    } catch (error) {
-      console.error('Error creating user:', error.response); // Log the error response
-      // Optionally, handle error response (e.g., display error messages)
-    }
-  };
-
   return (
-    <div className="d-flex justify-content-around">
-      <form onSubmit={handleSubmit} className="col-4 p-4 bg-dark text-light">
-        <h2 className="text-primary">Register</h2>
-        <input type="text" name="first_name" placeholder="First Name" value={formData.first_name} onChange={handleChange} className="form-control" />
-        <input type="text" name="last_name" placeholder="Last Name" value={formData.last_name} onChange={handleChange} className="form-control" />
-        <input type="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange} className="form-control" />
-        <input type="password" name="pw" placeholder="Password" value={formData.pw} onChange={handleChange} className="form-control" />
-        <input type="password" name="confirm_pw" placeholder="Confirm Password" value={formData.confirm_pw} onChange={handleChange} className="form-control" />
-        <select name="role" value={formData.role} onChange={handleChange} className="form-control">
-          <option value="">Select Role</option>
-          <option value="Nominator">Nominator</option>
-          <option value="Recommender">Recommender</option>
-          <option value="ReviewCommittee">ReviewCommittee</option>
-        </select>
-        <button type="submit" className="btn btn-primary">Register</button>
-      </form>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/recommender/dashboard" element={
+          <ProtectedRoute allowedRoles={['Recommender', 'Nominator']}>
+            <RecommenderDashboard />
+          </ProtectedRoute>
+        } />
+        <Route path="/review/dashboard" element={
+          <ProtectedRoute allowedRoles={['Review Committee']}>
+            <ReviewCommitteeDashboard />
+          </ProtectedRoute>
+        } />
+        <Route path="/nominee/dashboard" element={
+          <ProtectedRoute allowedRoles={['Nominator']}>
+            <NominatorDashboard />
+          </ProtectedRoute>
+        } />
+        <Route path="/nominee/new" element={<NewNomineeForm />} />
+        <Route path="/nominee/:id/edit" element={<EditNominee />} />
+        <Route path="/nominee/:id/info" element={<ViewNominee />} />
+        <Route path="/recommend/new" element={<RecommendNominee />} />
+        <Route path="/review/nominee/:nomineeId/details" element={<NomineeDetails />} />
+        <Route path="/nominee/:id/education" element={<NomineeEducationForm />} />
+        <Route path="/nominee/:id/education/edit" element={<EditEducationForm />} />
+      </Routes>
+    </Router>
   );
 }
 
